cc72ab12232d27ec4b8532799ec9362795d9a864
patb <user@server>
2014-05-13 02:20:31 +0530

    Basic UI done. Logic needs to be added!

--- TicTacToe/src/com/example/tictactoe/MainActivity.java
+++ TicTacToe/src/com/example/tictactoe/MainActivity.java
@@ -2,14 +2,43 @@ package com.example.tictactoe;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button button1, button2, button3, button4, button5, button6, button7, button8, button9, resetGame;
+	private int turn = 0;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		button1 = (Button) findViewById(R.id.button1);
+		button2 = (Button) findViewById(R.id.button2);
+		button3 = (Button) findViewById(R.id.button3);
+		button4 = (Button) findViewById(R.id.button4);
+		button5 = (Button) findViewById(R.id.button5);
+		button6 = (Button) findViewById(R.id.button6);
+		button7 = (Button) findViewById(R.id.button7);
+		button8 = (Button) findViewById(R.id.button8);
+		button9 = (Button) findViewById(R.id.button9);
+		resetGame = (Button) findViewById(R.id.resetGame);
+		
+		button1.setOnClickListener(this);
+		button2.setOnClickListener(this);
+		button3.setOnClickListener(this);
+		button4.setOnClickListener(this);
+		button5.setOnClickListener(this);
+		button6.setOnClickListener(this);
+		button7.setOnClickListener(this);
+		button8.setOnClickListener(this);
+		button9.setOnClickListener(this);
+		resetGame.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,6 +46,26 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		
+		try {
+			if (v.getId() == button1.getId()) {
+				if (turn == 0) {
+					button1.setText("O");
+					turn = 1;
+				}else{
+					button1.setText("X");
+					turn = 0;
+				}
+			}
+		} catch (Exception e) {
+			Log.d("Exception", e.getMessage());
+		}
+		
+		
 	}
 
 }
